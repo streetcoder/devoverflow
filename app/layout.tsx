49559926f475e61bd5060e4d3b26df7a94f9a0f2
streetcoder <streetkoder@gmail.com>
2025-03-16 15:40:12 -0600

    assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevOverFlow",
-  description: "A better version of Stack OverFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
