b5fcc1088df0d5776bf54c0c40142106ba11cfa7
streetcoder <streetkoder@gmail.com>
2025-03-24 17:28:50 -0600

    implement UI of the question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import QuestionsForm from "@/components/forms/QuestionsForm";
+
 const AskAQuestion = () => {
-  return <div>AskAQuestion</div>;
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+
+      <div className="mt-9">
+        <QuestionsForm />
+      </div>
+    </>
+  );
 };
 
 export default AskAQuestion;
