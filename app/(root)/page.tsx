7482e155a299124b992be044a4152908aa5ec988
streetcoder <streetkoder@gmail.com>
2025-04-19 15:58:56 -0600

    implement fetch and display questions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,116 +5,33 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-// import { api } from "@/lib/api";
-// import handleError from "@/lib/handlers/error";
-
-const questions = [
-  {
-    _id: "1",
-    title: "How to learn React?",
-    description: "I want to learn React? Can you help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "Javascript" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      image:
-        "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-    },
-    upvote: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date(),
-  },
-  {
-    _id: "2",
-    title: "What is Node.js used for?",
-    description: "Can someone explain the use cases of Node.js?",
-    tags: [
-      { _id: "3", name: "Node.js" },
-      { _id: "2", name: "Javascript" },
-    ],
-    author: {
-      _id: "2",
-      name: "Jane Smith",
-      image:
-        "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-    },
-    upvote: 15,
-    answers: 8,
-    views: 150,
-    createdAt: new Date(),
-  },
-  {
-    _id: "3",
-    title: "Best practices for MongoDB?",
-    description: "What are some best practices for using MongoDB efficiently?",
-    tags: [
-      { _id: "4", name: "MongoDB" },
-      { _id: "5", name: "Database" },
-    ],
-    author: {
-      _id: "3",
-      name: "Alice Brown",
-      image:
-        "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-    },
-    upvote: 20,
-    answers: 12,
-    views: 200,
-    createdAt: new Date(),
-  },
-  {
-    _id: "4",
-    title: "How to learn Javascript?",
-    description: "I want to learn Javascript? Can you help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "Javascript" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      image:
-        "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-    },
-    upvote: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date(),
-  },
-];
+import { getQuestions } from "@/lib/actions/question.action";
 
 interface SearchParams {
-  searchParams: Promise<{ [Key: string]: string }>;
+  searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query = "", filter = "" } = await searchParams;
+  const { page, pageSize, query, filter } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) => {
-    // Match query against the title
-    const matchesQuery = question.title
-      .toLowerCase()
-      .includes(query.toLowerCase());
-
-    // Match filter against tags or author name, adjust logic as needed
-    const matchesFilter = filter
-      ? question.tags.some(
-          (tag) => tag.name.toLowerCase() === filter.toLowerCase()
-        ) || question.author.name.toLowerCase() === filter.toLowerCase()
-      : true; // If no filter is provided, include all questions
-
-    return matchesQuery && matchesFilter;
+  const { success, data, error } = await getQuestions({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+    filter: filter || "",
   });
+
+  const { questions } = data || {};
 
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-        <Button>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
@@ -127,11 +44,25 @@ const Home = async ({ searchParams }: SearchParams) => {
         />
       </section>
       <HomeFilter />
-      <div className="mt-10 flex w-full flex-col gap-6">
-        {filteredQuestions.map((question) => (
-          <QuestionCard key={question._id} question={question} />
-        ))}
-      </div>
+      {success ? (
+        <div className="mt-10 flex w-full flex-col gap-6">
+          {questions && questions.length > 0 ? (
+            questions.map((question) => (
+              <QuestionCard key={question._id} question={question} />
+            ))
+          ) : (
+            <div className="mt-10 flex w-full items-center justify-center">
+              <p className="text-dark400_light700">No questions found</p>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="mt-10 flex w-full items-center justify-center">
+          <p className="text-dark400_light700">
+            {error?.message || "Failed to fetch questions"}
+          </p>
+        </div>
+      )}
     </>
   );
 };
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,11 +1,15 @@
 "use server";
 
-import mongoose from "mongoose";
+import mongoose, { FilterQuery } from "mongoose";
 
-import Question from "@/database/question.model";
+import Question, { IQuestionDoc } from "@/database/question.model";
 import TagQuestion from "@/database/tag-question.model";
 import Tag, { ITagDoc } from "@/database/tag.model";
-import { ActionResponse, ErrorResponse } from "@/types/global";
+import {
+  ActionResponse,
+  ErrorResponse,
+  PaginatedSearchParams,
+} from "@/types/global";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
@@ -13,6 +17,7 @@ import {
   AskQuestionSchema,
   EditQuestionSchema,
   GetQuestionSchema,
+  PaginatedSearchParamsSchema,
 } from "../validations";
 
 export async function createQuestion(
@@ -78,8 +83,8 @@ export async function createQuestion(
 }
 
 export async function editQuestion(
-  params: editQuestionParams
-): Promise<ActionResponse<Question>> {
+  params: EditQuestionParams
+): Promise<ActionResponse<IQuestionDoc>> {
   const validationResult = await action({
     params,
     schema: EditQuestionSchema,
@@ -98,24 +103,31 @@ export async function editQuestion(
 
   try {
     const question = await Question.findById(questionId).populate("tags");
+
     if (!question) {
       throw new Error("Question not found");
     }
+
     if (question.author.toString() !== userId) {
-      throw new Error("You are not authorized to edit this question");
+      throw new Error("Unauthorized");
     }
-    if (question.title !== title && question.content !== content) {
+
+    if (question.title !== title || question.content !== content) {
       question.title = title;
       question.content = content;
       await question.save({ session });
     }
 
     const tagsToAdd = tags.filter(
-      (tag) => !question.tags.includes(tag.toLowerCase())
+      (tag) =>
+        !question.tags.some((t: ITagDoc) =>
+          t.name.toLowerCase().includes(tag.toLowerCase())
+        )
     );
 
     const tagsToRemove = question.tags.filter(
-      (tag: ITagDoc) => !tags.includes(tag.name.toLowerCase())
+      (tag: ITagDoc) =>
+        !tags.some((t) => t.toLowerCase() === tag.name.toLowerCase())
     );
 
     const newTagDocuments = [];
@@ -123,17 +135,17 @@ export async function editQuestion(
     if (tagsToAdd.length > 0) {
       for (const tag of tagsToAdd) {
         const existingTag = await Tag.findOneAndUpdate(
-          {
-            name: { $regex: new RegExp(`^${tag}$`, "i") },
-          },
+          { name: { $regex: `^${tag}$`, $options: "i" } },
           { $setOnInsert: { name: tag }, $inc: { questions: 1 } },
           { upsert: true, new: true, session }
         );
+
         if (existingTag) {
           newTagDocuments.push({
-            question: questionId,
             tag: existingTag._id,
+            question: questionId,
           });
+
           question.tags.push(existingTag._id);
         }
       }
@@ -141,7 +153,8 @@ export async function editQuestion(
 
     if (tagsToRemove.length > 0) {
       const tagIdsToRemove = tagsToRemove.map((tag: ITagDoc) => tag._id);
-      await TagQuestion.updateMany(
+
+      await Tag.updateMany(
         { _id: { $in: tagIdsToRemove } },
         { $inc: { questions: -1 } },
         { session }
@@ -153,25 +166,26 @@ export async function editQuestion(
       );
 
       question.tags = question.tags.filter(
-        (tagId: mongoose.Types.ObjectId) => !tagIdsToRemove.includes(tagId)
+        (tag: mongoose.Types.ObjectId) =>
+          !tagIdsToRemove.some((id: mongoose.Types.ObjectId) =>
+            id.equals(tag._id)
+          )
       );
-
-      if (newTagDocuments.length > 0) {
-        await TagQuestion.insertMany(newTagDocuments, { session });
-      }
-
-      await question.save({ session });
-      await session.commitTransaction();
-      return {
-        success: true,
-        data: JSON.parse(JSON.stringify(question)),
-      };
     }
+
+    if (newTagDocuments.length > 0) {
+      await TagQuestion.insertMany(newTagDocuments, { session });
+    }
+
+    await question.save({ session });
+    await session.commitTransaction();
+
+    return { success: true, data: JSON.parse(JSON.stringify(question)) };
   } catch (error) {
     await session.abortTransaction();
     return handleError(error) as ErrorResponse;
   } finally {
-    session.endSession();
+    await session.endSession();
   }
 }
 
@@ -197,6 +211,75 @@ export async function getQuestion(
     return {
       success: true,
       data: JSON.parse(JSON.stringify(question)),
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getQuestions(
+  params: PaginatedSearchParams
+): Promise<ActionResponse<{ questions: Question[]; isNext: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: PaginatedSearchParamsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, filter } = params;
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = Number(pageSize);
+
+  const filterQuery: FilterQuery<typeof Question> = {};
+
+  if (filter === "recommended") {
+    return { success: true, data: { questions: [], isNext: false } };
+  }
+
+  if (query) {
+    filterQuery.$or = [
+      { title: { $regex: new RegExp(query, "i") } },
+      { content: { $regex: new RegExp(query, "i") } },
+    ];
+  }
+
+  let sortCriteria = {};
+
+  switch (filter) {
+    case "newest":
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "unanswered":
+      filterQuery.answers = 0;
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "popular":
+      sortCriteria = { upvotes: -1 };
+      break;
+    default:
+      sortCriteria = { createdAt: -1 };
+      break;
+  }
+
+  try {
+    const totalQuestions = await Question.countDocuments(filterQuery);
+
+    const questions = await Question.find(filterQuery)
+      .populate("tags", "name")
+      .populate("author", "name image")
+      .lean()
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalQuestions > skip + questions.length;
+
+    return {
+      success: true,
+      data: { questions: JSON.parse(JSON.stringify(questions)), isNext },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
