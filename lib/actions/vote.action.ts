6ccc5c2152ce67788afdfaf7fa9cea9eef8d545f
streetcoder <streetkoder@gmail.com>
2025-06-16 16:12:48 -0600

    fixed vote issues

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -73,7 +73,7 @@ export async function createVote(
   const { targetId, targetType, voteType } = validationResult.params!;
   const userId = validationResult.session?.user?.id;
 
-  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
 
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -100,6 +100,11 @@ export async function createVote(
           { voteType },
           { new: true, session }
         );
+        await updateVoteCount(
+          { targetId, targetType, voteType: existingVote.voteType, change: -1 },
+          session
+        );
+
         await updateVoteCount(
           { targetId, targetType, voteType, change: 1 },
           session
