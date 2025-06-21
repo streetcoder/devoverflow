bb1f9ea9993bdf3353197525c254dd2e6ed965c8
streetcoder <streetkoder@gmail.com>
2025-06-21 11:01:13 -0600

    developed update reputation logic

--- components/user/Stats.tsx
+++ components/user/Stats.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import { formatNumber } from "@/lib/utils";
+import { BadgeCounts } from "@/types/global";
 
 interface Props {
   totalAnswers: number;
   totalQuestions: number;
   badges: BadgeCounts;
+  reputationPoints: number;
 }
 
 interface StatsCardProps {
@@ -24,10 +26,20 @@ const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => (
   </div>
 );
 
-const Stats = ({ totalAnswers, totalQuestions, badges }: Props) => {
+const Stats = ({
+  totalAnswers,
+  totalQuestions,
+  badges,
+  reputationPoints,
+}: Props) => {
   return (
     <div className="mt-3">
-      <h4 className="h3-semibold text-dark200_light900">Stats</h4>
+      <h4 className="h3-semibold text-dark200_light900">
+        Stats{" "}
+        <span className="small-semibold primary-text-gradient">
+          {formatNumber(reputationPoints)}
+        </span>
+      </h4>
 
       <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 gal-4 flex flex-wrap items-center justify-evenly rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
--- lib/validations.ts
+++ lib/validations.ts
@@ -1,3 +1,4 @@
+import { InteractionActionEnums } from "@/database/interaction.model";
 import { z } from "zod";
 
 export const SignInSchema = z.object({
@@ -218,4 +219,11 @@ export const DeleteQuestionSchema = z.object({
 
 export const DeleteAnswerSchema = z.object({
   answerId: z.string().min(1, "Answer ID is required"),
+});
+
+export const CreateInteractionSchema = z.object({
+  action: z.enum(InteractionActionEnums),
+  actionTarget: z.enum(["question", "answer"]),
+  actionId: z.string().min(1),
+  authorId: z.string().min(1),
 });
